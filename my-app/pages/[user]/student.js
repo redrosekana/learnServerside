2864aa50b664a2a0dd82e6957bda2d1fb26df7cb
kana <lnbtkana2326@gmail.com>
2022-09-01 17:12:48 +0700

    folder dynamic

---
+++ my-app/pages/[user]/student.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+/* ตัวอย่างการทำ dynamic ด้วยโฟลเดอร์ */
+export default function Student(props) {
+  return (
+    <div>
+        <p>{props.data.id} {props.data.name}</p>
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+    let a = [
+        { 'params': { 'user': '1' } }, 
+        { 'params': { 'user': '2' } }, 
+        { 'params': { 'user': '3' } }, 
+        { 'params': { 'user': '4' } }, 
+        { 'params': { 'user': '5' } }
+    ]
+    console.log(a)
+    return {
+        paths:a,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps(context) {
+    const response = await fetch(`http://127.0.0.1:8000/user/${context.params.user}`)
+    const data = await response.json()
+    if (context.params.user === "3"){
+        return {
+            notFound:true
+        }
+    }else{
+        return {
+            props: {data}
+        }
+    }
+
+    
+}
+
